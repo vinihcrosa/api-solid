dd1c92fda917865f0d5c31555753951334f3c252
Vinicius Rosa <viniciusrosa@MacBook-Pro-de-Vinicius.local>
2024-02-11 16:53:35 -0300

    chore: config env file

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import { app } from "./app";
+import { env } from './env'
 
 app.listen({
-  port: 3000,
+  port: env.PORT,
   host: '0.0.0.0'
 }).then((address) => {
   console.log(`Server listening on ${address}`);
